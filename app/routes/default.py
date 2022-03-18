618505c6e26dda0c98b4cb3117117fae3d755460
EricNguyen1213 <eric.nguyen1213@gmail.com>
2022-03-18 10:11:55 -0700

    done

--- app/routes/default.py
+++ app/routes/default.py
@@ -7,9 +7,5 @@ def index():
     return render_template('index.html')
 
 @app.route('/aboutus')
-<<<<<<< HEAD
-def test():
-=======
 def aboutus():
->>>>>>> a08386a94239d8d234ae0d5a1ef66aa9d43ea93c
     return render_template('aboutus.html')
